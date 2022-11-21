27c92c45044606f1df53a32291b964b4606f96cf
Enismasic <emasic@hawk.iit.edu>
2022-11-21 05:11:19 -0600

    added code to js and fixed map issue

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
+  var map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: -34.397, lng: 150.644 },
     zoom: 8,
   });
+  
+  var marker = new google.maps.Marker({
+    position: { lat: -34.397, lng: 150.644},
+    map: map,
+  });
+  
+  var infowindow = new google.maps.InfoWindow({
+    content:"Navigate using Google Maps!"
+  });
+  infowindow.open(map, marker);
 }
-
-var marker = new google.maps.Marker({
-	position: {  lat: -34.397, lng: 150.644 }, 
-	map: map,
-});
 
 window.initMap = initMap;
