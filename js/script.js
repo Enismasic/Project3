53c2d7739b1299cfc55c53e8a0bffa9f7faeff8e
Enismasic <emasic@hawk.iit.edu>
2022-11-22 00:59:55 -0600

    removed salmon color, and added more controls to js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
 function initMap() {
   var map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+    center: { lat: 41.879064, lng: -87.635936 },
+    zoom: 15,
+	zoomControl: false,
+    scaleControl: true,
+	mapTypeId: 'hybrid'
   });
   
   var marker = new google.maps.Marker({
-    position: { lat: -34.397, lng: 150.644},
+    position: { lat: 41.879064, lng: -87.635936},
     map: map,
   });
   
   var infowindow = new google.maps.InfoWindow({
-    content:"Navigate using Google Maps!"
+    content:"The Famous Willis Tower in Chicago"
   });
   infowindow.open(map, marker);
 }
